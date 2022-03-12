8ce3e9177538021852a2ce94300053cedf8fce4e
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-03-12 11:19:52 +0530

    updated

--- StringsandDictionaries.py
+++ StringsandDictionaries.py
@@ -1,2 +1,45 @@
 """Strings and Dictionaries
 Working with strings and dictionaries, two fundamental Python data types"""
+
+##String syntax
+
+x = 'Pluto si a planet'
+y = "Pluto is a planet"
+
+print("Pluto\"s  is a planet")
+
+"""The table below summarizes some important uses of the backslash character.
+
+What you type...	What you get	example	print(example)
+\'	'	'What\'s up?'	What's up?
+\"	"	"That's \"cool\""	That's "cool"
+\\	\	"Look, a mountain: /\\"	Look, a mountain: /\
+\n	
+"1\n  2 3" 1 2 3
+"""
+
+print("That's \"cool\" ")
+
+print("That is \ncooler")
+
+print("hello", end=' ')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
