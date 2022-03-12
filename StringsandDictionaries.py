f4a0483b91b2c55cbaa996c7f21d8788ce2d9ca4
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-03-12 11:54:50 +0530

    updated the string join and split

--- StringsandDictionaries.py
+++ StringsandDictionaries.py
@@ -22,13 +22,51 @@ print("That's \"cool\" ")
 
 print("That is \ncooler")
 
-print("hello", end=' ')
+print("hello cool",end=" ")
+print("cool")
+print("cool")
 
 
+"""Strings are sequences"""
 
+planet = "pluto"
 
+print(planet[2])
 
+print(planet[:-1])
+# slicing
+print(planet[-3:])
+print(len(planet))
 
+print([char+'!' for char in planet])
+
+"""" string does not support the modify them """
+# planet[0]='b'
+# print(planet)
+
+""" String Methods """
+
+course = "python is a programming"
+
+print(course.upper())
+
+print(course.lower())
+
+print(course.index("th"))
+
+print(course.startswith("py"))
+
+print(course.endswith("on"))
+
+""" Strings and list have function ----> .split() and .join()"""
+
+date = '1955-01-31'
+year, month, day = date.split('-')
+print(year,month,day)
+
+print('/'.join([month,day,year]))
+
+print('🐍'.join([world.upper() for world in course ]))
 
 
 
