a1821ba6e3b0eebd57e4e39df601452e0affd9da
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-03-15 10:37:02 +0530

    completed

--- PythonOOPS.py
+++ PythonOOPS.py
@@ -41,7 +41,63 @@ p2.myfunc()
 
 del p2
 
-##
+## the pass statement ---> class definitions cannot be empty, but if you for some reson have a class
+## definition with no content, put in the pass statement to avoid getting an error.
+
+class p1s:
+    pass
+
+
+""" Python Inheritance """
+
+
+""" Inheritance allows us to define a class that inherits all the methods and properties from another class.
+
+Parent class is the class being inherited from, also called base class.
+
+Child class is the class that inherits from another class, also called derived class. 
+"""
+
+## creating a parent class
+
+class person:
+    def __init__(self,fname,lname):
+        self.firstname = fname
+        self.lastname = lname
+
+    def printname(self):
+        print(self.firstname,self.lastname)
+
+x = person("hone","doe")
+
+x.printname()
+
+## creating a child class
+
+class student(person):
+    pass
+
+## __init__() function
+
+class students(person):
+
+    def __init__(self,fname,lname):
+        super().__init__(fname,lname)
+
+
+x2 = students("mmm","olen")
+
+print(x2.firstname)
+print(x2.lastname)
+
+
+
+
+
+
+
+
+
 
 
 
