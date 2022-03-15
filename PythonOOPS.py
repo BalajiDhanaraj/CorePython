d4178700d2a2d25d06c28ae5580777b995293ed2
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-03-15 10:23:13 +0530

    completed

--- PythonOOPS.py
+++ PythonOOPS.py
@@ -17,3 +17,33 @@ class Myclass:
 p1 = Myclass("john",33)
 
 print(p1.name,p1.age)
+
+### self is not name self , so we can name what ever we want
+
+class Myclass2:
+
+    def __init__(s1,name="mike",age=55):
+        s1.name = name
+        s1.age = age
+    def myfunc(s2):
+        print("hello my name is ",s2.name," ",s2.age)
+
+p2 = Myclass2("walker",22)
+
+p2 = Myclass2()
+"""p2.name = "scence"
+p2.age = 23
+"""
+p2.myfunc()
+
+
+## delete object using  " del "
+
+del p2
+
+##
+
+
+
+
+
